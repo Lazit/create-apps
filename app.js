c41bf5f4a7bc0745e5fab7ea53913e999bac93c2
Lazit <31299536+Lazit@users.noreply.github.com>
2022-04-13 00:11:29 +0900

    login end

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 const logout = document.querySelector(".logout");
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+//임의로 사용된 string은 실수 시 경고없이 무반응.
+//변수로 집어주면 경고를 줘서 훨씬 좋다!!!
 
 function onLoginSubmit(event) {
-    event.preventDefault();
+    // event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     //greeting.innerText = "Hello " + username;
-    localStorage.setItem("username", username)
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(savedUsername);
     // console.log(loginInput.value);
 }
 
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.log(event); 
-}
 
-
+//임의작성
 function handleLogOut(event) {
     const username = loginInput.value;
-    localStorage.removeItem("username", username)
+    localStorage.removeItem(USERNAME_KEY, username)
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     greeting.classList.add(HIDDEN_CLASSNAME);
+    logout.classList.add(HIDDEN_CLASSNAME)
+    location.reload()//새로고침
+}
+//임의작성 끝
+
+
+function paintGreetings(username) {
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
 logout.addEventListener("click", handleLogOut);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings
+    paintGreetings(savedUsername);
+    logout.classList.remove(HIDDEN_CLASSNAME)
+}
