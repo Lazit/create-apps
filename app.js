c027e3ad56ea63571fb652dfeed97012cf05b03e
Lazit <31299536+Lazit@users.noreply.github.com>
2022-04-12 22:47:40 +0900

    local storage part1

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const link = document.querySelector("a");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
+const logout = document.querySelector(".logout");
 
 const HIDDEN_CLASSNAME = "hidden"
 
@@ -11,6 +12,7 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     //greeting.innerText = "Hello " + username;
+    localStorage.setItem("username", username)
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     // console.log(loginInput.value);
@@ -22,5 +24,14 @@ function handleLinkClick(event) {
     console.log(event); 
 }
 
+
+function handleLogOut(event) {
+    const username = loginInput.value;
+    localStorage.removeItem("username", username)
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
+logout.addEventListener("click", handleLogOut);
