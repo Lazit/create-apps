7c15a6e6f4276e4be24cb51dbfa23c672befa98d
Lazit <31299536+Lazit@users.noreply.github.com>
2022-04-17 23:27:31 +0900

    todo saved data copy into an array

--- js/todo.js
+++ js/todo.js
@@ -5,7 +5,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos"
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)) //todos는 로컬스토리지에 기록될 이름, JSON~은 값을 문자로 만들어줌. JSON.parse 와 짝꿍.
@@ -51,11 +51,14 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     //자바스크립트는 array 안의 각각의 item 마다 각각의 function을 사용할 수 있게 해줌.
+    toDos = parsedToDos;
     parsedToDos.forEach(paintToDo)
     //각각의 아이템에 대해 foreach 안의 함수를 실행해라.
     // console.log("sayhello") 함수 만들고 foreach 문 안에 넣어서 테스트
     // parsedToDos.forEach((item) => console.log("This is the turn of", item));   //함수 short cut
     //paintToDo만 호출하면, 기존의 것은 저장을 못하고 새로운 값만 저장해서 보여줌.
     //위의 원인은 시작할때 8번줄에서 array가 빈 상태로 시작해서 그럼
+    //newTodo 만 저장해서 그럼. 이전 데이터의 복사본이 없음.
+    //해결 = 54번줄 추가
 
 }
